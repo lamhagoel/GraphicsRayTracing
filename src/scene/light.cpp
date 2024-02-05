72ecccab10c8780ca7f48cb2eb7a74dce57dc6e8
sebastianwgm <122843474+sebastianwgm@users.noreply.github.com>
2024-02-05 14:50:19 -0600

    Update light.cpp

--- src/scene/light.cpp
+++ src/scene/light.cpp
@@ -16,33 +16,37 @@ glm::dvec3 DirectionalLight::shadowAttenuation(const ray &r,
                                                const glm::dvec3 &p) const {
   // YOUR CODE HERE:
   // You should implement shadow-handling code here.
-  ray new_ray(r);
-	isect i;
-	// check if we have intersection
-	if (getScene()->intersect (new_ray, i)) {
-		// now we check if the distance point to light is less than distance point to intersection
-		glm::dvec3 point = new_ray.at(i.getT());
-			// check if the object is translucent
-		if (i.getMaterial().Trans()) {
-			// shoot new ray 
-			ray r_trans(new_ray);
-			r_trans.setPosition(point);
-			// second intersection
-			isect iItranlucid;
-			// confirm we have second intersection
-			if (getScene()->intersect(r_trans, iItranlucid)) {
-				// TODO: confirm if we get the distance like this
-				double d = glm::distance(iItranlucid.getT(), i.getT());
-				// double d = iItranlucid.getT();
-				glm::dvec3 multiply = glm::pow (i.getMaterial().kt(i), glm::dvec3(d, d, d));
-				r_trans.setPosition(r_trans.at(iItranlucid));
-				return multiply * shadowAttenuation(r_trans, r_trans.at(iItranlucid));
-			}
-		}
-		return glm::dvec3(0, 0, 0);
-	}
+  	
+	// ray new_ray(r);
+	// isect i;
+	// // check if we have intersection
+	// if (getScene()->intersect (new_ray, i)) {
+	// 	// now we check if the distance point to light is less than distance point to intersection
+	// 	glm::dvec3 point = new_ray.at(i.getT());
+	// 		// check if the object is translucent
+	// 	if (i.getMaterial().Trans()) {
+	// 		// shoot new ray 
+	// 		ray r_trans(new_ray);
+	// 		r_trans.setPosition(point);
+	// 		// second intersection
+	// 		isect iItranlucid;
+	// 		// confirm we have second intersection
+	// 		if (getScene()->intersect(r_trans, iItranlucid)) {
+	// 			// TODO: confirm if we get the distance like this
+	// 			// double d = glm::distance(iItranlucid.getT(), i.getT());
+	// 			// changed:
+	// 			// double d = glm::distance(point, p);
+	// 			double d = iItranlucid.getT();
+	// 			glm::dvec3 multiply = glm::pow (i.getMaterial().kt(i), glm::dvec3(d, d, d));
+	// 			r_trans.setPosition(r_trans.at(iItranlucid));
+	// 			return multiply * shadowAttenuation(r_trans, r_trans.at(iItranlucid));
+	// 		}
+	// 	}
+	// 	return glm::dvec3(0, 0, 0);
+	// }
 
 	return glm::dvec3(1,1,1);
+	
 }
 
 glm::dvec3 DirectionalLight::getColor() const { return color; }
@@ -66,12 +70,11 @@ double PointLight::distanceAttenuation(const glm::dvec3 &P) const {
   // float quadraticTerm; // c
   double d = glm::length(position - P);
   double result;
-  if (1/(constantTerm + linearTerm * d + quadraticTerm * pow(d, 2)) > 1) {
-    result = 1;
+  if (1.0/(constantTerm + linearTerm * d + quadraticTerm * pow(d, 2.0)) > 1) {
+    result = 1.0;
   } else {
-    result =  1/(constantTerm + linearTerm * d + quadraticTerm * pow(d, 2));
+    result =  1.0/(constantTerm + linearTerm * d + quadraticTerm * pow(d, 2.0));
   }
-  // double result = std::min(1.0, 1/(constantTerm + linearTerm * d + quadraticTerm * pow(d, 2)));
   return result;
 }
 
@@ -83,37 +86,35 @@ glm::dvec3 PointLight::getDirection(const glm::dvec3 &P) const {
 
 glm::dvec3 PointLight::shadowAttenuation(const ray &r,
                                          const glm::dvec3 &p) const {
-  // YOUR CODE HERE:
-  // You should implement shadow-handling code here.
-  
-  // TODO: handle when the light reflect inside the object?
-	ray new_ray(r);
+	// TODO: handle when the light reflect inside the object?
+	ray new_ray = r;
 	isect i;
 	// check if we have intersection
 	if (getScene()->intersect (new_ray, i)) {
 		// now we check if the distance point to light is less than distance point to intersection
 		glm::dvec3 point = new_ray.at(i.getT());
-		double d1 = glm::distance(point, p);
+		double d1 = glm::distance(p, point);
 		double d2 = glm::distance(position, p);
 		if (d1 <= d2) {
 			// check if the object is translucent
 			if (i.getMaterial().Trans()) {
 				// shoot new ray
 				ray r_trans(new_ray);
-				r_trans.setPosition(point);
+				r_trans.setPosition(point);	
 				// second intersection
 				isect iItranlucid;
 				// confirm we have second intersection
 				if (getScene()->intersect(r_trans, iItranlucid)) {
 					// TODO: confirm if we get the distance like this
-					double d = glm::distance(iItranlucid.getT(), i.getT());
-					// double d = iItranlucid.getT();
+					// double d = glm::distance(iItranlucid.getT(), i.getT());
+					// changed:
+					double d = iItranlucid.getT();
+					// double d = glm::distance(point, p);
 					glm::dvec3 multiply = glm::pow (i.getMaterial().kt(i), glm::dvec3(d, d, d));
 					r_trans.setPosition(r_trans.at(iItranlucid));
 					return multiply * shadowAttenuation(r_trans, r_trans.at(iItranlucid));
-				}
+				} 
 			}
-			return glm::dvec3(0, 0, 0);		
 		}
 	}
 
