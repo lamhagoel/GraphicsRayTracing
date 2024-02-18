e9d0ba86cb8ac9b745321c15016e3ba70be47b9f
sebastianwgm <122843474+sebastianwgm@users.noreply.github.com>
2024-02-17 20:09:31 -0600

    Update cubeMap.cpp

--- src/scene/cubeMap.cpp
+++ src/scene/cubeMap.cpp
@@ -7,7 +7,79 @@ extern TraceUI *traceUI;
 glm::dvec3 CubeMap::getColor(ray r) const {
   // YOUR CODE HERE
   // FIXME: Implement Cube Map here
-  return glm::dvec3();
+  double x = r.getDirection().x;
+  double y = r.getDirection().y;
+  double z = - r.getDirection().z;
+  double absX = fabs(x);
+  double absY = fabs(y);
+  double absZ = fabs(z);
+  int index;
+  double u, v;
+  double maxAxis, uc, vc;
+  
+  int isXPositive = x > 0 ? 1 : 0;
+  int isYPositive = y > 0 ? 1 : 0;
+  int isZPositive = z > 0 ? 1 : 0;
+
+  // POSITIVE X
+  if (isXPositive && absX >= absY && absX >= absZ) {
+    // u (0 to 1) goes from +z to -z
+    // v (0 to 1) goes from -y to +y
+    maxAxis = absX;
+    uc = -z;
+    vc = y;
+    index = 0;
+  }
+  // NEGATIVE X
+  if (!isXPositive && absX >= absY && absX >= absZ) {
+    // u (0 to 1) goes from -z to +z
+    // v (0 to 1) goes from -y to +y
+    maxAxis = absX;
+    uc = z;
+    vc = y;
+    index = 1;
+  }
+  // POSITIVE Y
+  if (isYPositive && absY >= absX && absY >= absZ) {
+    // u (0 to 1) goes from -x to +x
+    // v (0 to 1) goes from +z to -z
+    maxAxis = absY;
+    uc = x;
+    vc = -z;
+    index = 2;
+  }
+  // NEGATIVE Y
+  if (!isYPositive && absY >= absX && absY >= absZ) {
+    // u (0 to 1) goes from -x to +x
+    // v (0 to 1) goes from -z to +z
+    maxAxis = absY;
+    uc = x;
+    vc = z;
+    index = 3;
+  }
+  // POSITIVE Z
+  if (isZPositive && absZ >= absX && absZ >= absY) {
+    // u (0 to 1) goes from -x to +x
+    // v (0 to 1) goes from -y to +y
+    maxAxis = absZ;
+    uc = x;
+    vc = y;
+    index = 4;
+  }
+  // NEGATIVE Z
+  if (!isZPositive && absZ >= absX && absZ >= absY) {
+    // u (0 to 1) goes from +x to -x
+    // v (0 to 1) goes from -y to +y
+    maxAxis = absZ;
+    uc = -x;
+    vc = y;
+    index = 5;
+  }
+
+  // Convert range from -1 to 1 to 0 to 1
+  u = 0.5 * (uc / maxAxis + 1.0);
+  v = 0.5 * (vc / maxAxis + 1.0);
+  return tMap[index]->getMappedValue(glm::dvec2(u,v));
 }
 
 CubeMap::CubeMap() {}
